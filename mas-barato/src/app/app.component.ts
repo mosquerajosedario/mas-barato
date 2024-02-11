758c5f7438a0810d730844cd5b21fd7f64883e6c
mosquerajosedario@gmail.com <salinascatalina156@gmail.com>
2024-02-11 11:17:06 -0300

    Integracion bootstra, todavia estamos creando primer interface

---
+++ mas-barato/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'mas-barato';
+
+  constructor(private modalService: NgbModal) {
+  }
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
+}
